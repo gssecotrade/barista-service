2ffa834f5c80e17a2db4b0486b27f55d72da65f1
Francisco Cabrera <franciscocabreraya@gmail.com>
2026-04-27 14:44:59 +0200

    fisprisma connection config

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -4,12 +4,19 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const prisma =
+const prismaClient =
   globalForPrisma.prisma ??
   new PrismaClient({
     log: ["error", "warn"],
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
   });
 
+export const prisma = prismaClient;
+
 if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
+  globalForPrisma.prisma = prismaClient;
 }
